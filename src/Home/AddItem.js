a7507d4c77f7336d468d7bf92f22ddf8f1000906
bshoop <braxton.shoop@gmail.com>
2020-12-21 14:15:26 -0600

    add items fields created

--- src/Home/AddItem.js
+++ src/Home/AddItem.js
@@ -13,42 +13,74 @@ export function AddItem() {
             </header>
 
             <div className="BodyContainer">
-                <div className="FiltersContainer">
-                    <div className="RoomContainer">
-                        <label className="RoomFilterTitle">Room </label>
-                        <select className="RoomFilter">Room
-                            <option>1</option>
-                            <option>2</option>
+                <div className="InputsContainer">
+                    <div className="RoomInputContainer">
+                        <label className="RoomInputTitle">Room</label>
+                        <select className="RoomInput">
+                            <option selected="selected"></option>
+                            <option>Attic</option>
+                            <option>Back Yard</option>
+                            <option>Bed Room 1</option>
+                            <option>Bed Room 1 Closet</option>
+                            <option>Bed Room 2</option>
+                            <option>Bed Room 2 Closet</option>
+                            <option>Bed Room 3</option>
+                            <option>Bed Room 3 Closet</option>
+                            <option>Bonus Room</option>
+                            <option>Dining Room</option>
+                            <option>Downstairs Bathroom</option>
+                            <option>Downstairs Closet</option>
+                            <option>Foyer</option>
+                            <option>Front Porch</option>
+                            <option>Front Yard</option>
+                            <option>Garage</option>
+                            <option>Guest Bathroom</option>
+                            <option>Kitchen</option>
+                            <option>Laundry Room</option>
+                            <option>Living Room</option>
+                            <option>Main Bathroom</option>
+                            <option>Mud Room</option>
+                            <option>Patio</option>
+                            <option>Stairwell</option>
                         </select>
                     </div>
 
-                    <div className="ItemNumberContainer">
-                        <label className="ItemNumberTitle">Item Number</label>
-                        <select className="ItemNumberFilter">Item Inventory
-                            <option>1</option>
-                            <option>2</option>
-                        </select>
+                    <div className="ItemNameContainer">
+                        <label className="ItemNameTitle">Item Name:  </label>
+                        <input type="text" className="ItemNameInput" />
+                    </div>
+                    <label className="DescriptionTitle">Item Description:  </label>
+                    <div className="DescriptionContainer">
+
+                        <textarea type="textarea" className="DescriptionInput" />
                     </div>
 
-                    <div className="SearchContainer">
-                        <label className="SearchTitle">Search</label>
-                        <input type="text" className="Search" placeholder="Search  for an item" />
+                    <div className="ItemSerialContainer">
+                        <label className="ItemSerialTitle">Item Serial Number:  </label>
+                        <input type="text" className="ItemSerialInput" />
+                    </div>
+
+                    <label className="ItemNotesTitle">Item Notes:  </label>
+                    <div className="ItemNotesContainer">
+                        <textarea type="textarea" className="ItemNotesInput" />
                     </div>
                 </div>
 
-                <button className="SaveAddItem"
-                    onClick={() => {
-                        history.push(`/`)
-                    }}
-                    type="button">Save New Item
-                </button>
+                <div className="AddItemButtonsContainer">
+                    <button className="SaveAddItem"
+                        onClick={() => {
+                            history.push(`/`)
+                        }}
+                        type="button">Save New Item
+                    </button>
 
-                <button className="CancelAddItem"
-                    onClick={() => {
-                        history.push(`/`)
-                    }}
-                    type="button">Cancel
-                </button>
+                    <button className="CancelAddItem"
+                        onClick={() => {
+                            history.push(`/`)
+                        }}
+                        type="button">Cancel
+                    </button>
+                </div>
             </div>
         </>
     );
