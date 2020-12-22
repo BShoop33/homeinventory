10fb567de53c7683909e3235bd4cc72cbd60f79a
bshoop <braxton.shoop@gmail.com>
2020-12-21 22:12:16 -0600

    item cards now displaying

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,10 +1,60 @@
-import React from "react"
-import { useHistory } from "react-router-dom"
+import React, { useContext, useEffect, useState } from "react"
+import { ItemContext } from "./AppDataProvider.js"
+import { useHistory, useParams } from "react-router-dom"
 import './AddItem.css';
 
 export function AddItem() {
-
+    const [item, setNewItem] = useState({})
     const history = useHistory();
+    const { getItems, getItemById, editItems, addItems } = useContext(ItemContext)
+    const { itemId } = useParams()
+    const [isLoading, setIsLoading] = useState(true)
+
+    const handleControlledInputChange = (event) => {
+        const addedItem = { ...item }
+        addedItem[event.target.name] = event.target.value
+        setNewItem(addedItem)
+    }
+
+    // useEffect(() => {
+    //     getItems().then(() => {
+    //         if (itemId) {
+    //             getItemById(itemId)
+    //                 .then(item => {
+    //                     setNewItem(item)
+    //                     // setIsLoading(false)
+    //                 })
+    //         } else {
+    //             setIsLoading(false)
+    //         }
+    //     })
+    // }, [])
+
+    const constructItemObject = () => {
+        // setIsLoading(true)
+        // if (itemId) {
+        //     editItems({
+        //         id: item.id,
+        //         itemNumber: item.id,
+        //         itemName: item.ItemNameInput,
+        //         itemRoom: item.RoomInput,
+        //         itemDescription: item.itemDescription,
+        //         itemSerialNumber: item.itemSerialNumber,
+        //         itemNotes: item.itemNotes
+        //     })
+        //         .then(() => history.push("/"))
+        // } else {
+        addItems({
+
+            itemName: item.ItemNameInput,
+            itemRoom: item.RoomInput,
+            itemDescription: item.itemDescription,
+            itemSerialNumber: item.itemSerialNumber,
+            itemNotes: item.itemNotes
+        })
+            .then(() => history.push("/"))
+        // }
+    }
 
     return (
         <>
@@ -16,7 +66,7 @@ export function AddItem() {
                 <div className="InputsContainer">
                     <div className="RoomInputContainer">
                         <label className="RoomInputTitle">Room</label>
-                        <select className="RoomInput">
+                        <select className="RoomInput" name="RoomInput" onChange={handleControlledInputChange}>
                             <option selected="selected"></option>
                             <option>Attic</option>
                             <option>Back Yard</option>
@@ -47,28 +97,29 @@ export function AddItem() {
 
                     <div className="ItemNameContainer">
                         <label className="ItemNameTitle">Item Name:  </label>
-                        <input type="text" className="ItemNameInput" />
+                        <input type="text" className="ItemNameInput" name="ItemNameInput" onChange={handleControlledInputChange} />
                     </div>
                     <label className="DescriptionTitle">Item Description:  </label>
                     <div className="DescriptionContainer">
-
-                        <textarea type="textarea" className="DescriptionInput" />
+                        <textarea type="textarea" className="DescriptionInput" name="itemDescription" onChange={handleControlledInputChange} />
                     </div>
 
                     <div className="ItemSerialContainer">
                         <label className="ItemSerialTitle">Item Serial Number:  </label>
-                        <input type="text" className="ItemSerialInput" />
+                        <input type="text" className="ItemSerialInput" name="itemSerialNumber" onChange={handleControlledInputChange} />
                     </div>
 
                     <label className="ItemNotesTitle">Item Notes:  </label>
                     <div className="ItemNotesContainer">
-                        <textarea type="textarea" className="ItemNotesInput" />
+                        <textarea type="textarea" className="ItemNotesInput" name="itemNotes" onChange={handleControlledInputChange} />
                     </div>
                 </div>
 
                 <div className="AddItemButtonsContainer">
                     <button className="SaveAddItem"
-                        onClick={() => {
+                        onClick={item => {
+                            item.preventDefault()
+                            constructItemObject()
                             history.push(`/`)
                         }}
                         type="button">Save New Item
--- src/ApplicationView.js
+++ src/ApplicationView.js
@@ -2,17 +2,29 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { HomePage } from "./App";
 import { AddItem } from "./AddItem";
+import { ItemProvider } from "./AppDataProvider"
+import { ItemCard } from "./ItemCard"
 
 export default function ApplicationViews() {
     return (
         <>
-            <Route exact path="/">
-                <HomePage />
-            </Route>
+            <ItemProvider>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+            </ItemProvider>
 
-            <Route exact path="/additem">
-                <AddItem />
-            </Route>
+            {/* <ItemProvider>
+                <Route exact path="/">
+                    <ItemCard />
+                </Route>
+            </ItemProvider> */}
+
+            <ItemProvider>
+                <Route exact path="/additem">
+                    <AddItem />
+                </Route>
+            </ItemProvider>
         </>
     );
 }
